68b544067f87316b8252c08156aa1e26549557f9
Connor Christie <christieck@msoe.edu>
2018-02-13 22:00:27 -0600

    Use env vars

--- updater/update_pusher.js
+++ updater/update_pusher.js
@@ -1,18 +1,20 @@
 var Registry = require('azure-iothub').Registry;
 var Client = require('azure-iothub').Client;
 
-var connectionString = 'HostName=DAB-Hub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=l3mfQe03nobu0/Hmu7VnsdEOc6afufSmV0QOjiwLtZY=';
+var connectionString = process.env.AZURE_IOT_CONNECTION;
 var registry = Registry.fromConnectionString(connectionString);
 var client = Client.fromConnectionString(connectionString);
 var deviceToUpdate = 'dabOne';
 
 var startFirmwareUpdateDevice = function () {
-    let version = process.argv[2] || '1.2';
+    let username = process.env.GITHUB_USER;
+    let repo = process.env.GITHUB_REPO;
+    let version = process.argv[2];
 
     let params = {
         gh: {
-            username: 'ConnorChristie',
-            project: 'pinger'
+            username: username,
+            project: repo
         },
         version: version
     };
@@ -25,9 +27,14 @@ var startFirmwareUpdateDevice = function () {
 
     client.invokeDeviceMethod(deviceToUpdate, methodParams, function (err, result) {
         if (err) {
-            console.log(err);
-            console.error('Could not start the firmware update on the device: ' + err.message)
+            console.error('Could not start the firmware update on the device: ' + err.message);
+
+            process.exit(1);
+
+            return;
         }
+
+        console.log(`Successfully deployed ${username}/${repo} v${version} to ${deviceToUpdate}`);
     });
 };
 
